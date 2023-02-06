3bc67f253a2778cb9f3be405e1b2d4943cae01ec
Ajay Rathod <ajayrathod1137@gmail.com>
2023-02-07 01:00:06 +0530

    Adding files to projects

---
+++ app/src/main/java/com/example/explicitintent/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.explicitintent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btnIntent= findViewById<Button>(R.id.button)
+        btnIntent.setOnClickListener {
+
+            intent=Intent(applicationContext, SecondAct::class.java)
+            startActivity(intent)
+        }
+    }
+}
